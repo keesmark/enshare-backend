c65f461219ba7bf0f441ec03309e8cc52db61035
katsu <keesmark@gmail.com>
2019-09-19 21:45:06 -0600

    controller added

--- app/controllers/api/v1/gears_controller.rb
+++ app/controllers/api/v1/gears_controller.rb
@@ -1,9 +1,14 @@
 module Api::V1
   class GearsController < ApplicationController
+    before_action :set_gear, only: [:show, :edit, :update, :destroy]
     def index
       @gears = Gear.all.order(created_at: :desc)
 
       render json: @gears
+    end
+
+    def show
+      render json: @gear
     end
 
     def create
@@ -14,12 +19,30 @@ module Api::V1
         render json: @gear.errors
       end
     end
+    def update
+      if @gear.update(gear_params)
+        render json: @gear
+      else
+        render json: @gear.error
+      end
+    end
 
-    def destroy; end
+    def destroy
+      @gear.destroy
+      if @gear.destroy
+        head :no_content, status: :ok
+      else
+        render json: @gear.error
+      end
+    end
 
     private
     def gear_params
       params.require(:gear).permit(:title, :amazonUrl, :imgUrl, :content)
+    end
+
+    def set_gear
+      @gear = Gear.find(params[:id])
     end
   end
 end
